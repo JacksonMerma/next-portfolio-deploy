b075b8238d056de84807cbe34c8ccd41cec0c86c
Jackson Merma <mermaportocarreroj@gmail.com>
2023-10-19 18:27:48 -0500

    insertando data en secciones de about me usando el componente TabButton e imagenes de prueba para sección de proyectos, añadida

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,10 +1,47 @@
 "use client"
 import React, { useTransition, useState } from 'react'
 import Image from 'next/image'
+import TabButton from './TabButton'
+
+const TAB_DATA = [
+    {
+        title: "Skills",
+        id: "skills",
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>Tool 1</li>
+                <li>Tool 2</li>
+                <li>Tool 3</li>
+                <li>Tool 4</li>
+            </ul>
+        )
+    },
+    {
+        title: "Education",
+        id: "education",
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>collage 1</li>
+                <li>collage 2</li>
+            </ul>
+        )
+    },
+    {
+        title: "Certifications",
+        id: "certifications",
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>certification 1</li>
+                <li>certification 2</li>
+                <li>certification 3</li>
+            </ul>
+        )
+    }
+]
 
 const AboutSection = () => {
     const [tab, setTab] = useState("skills")
-    const [startTransition, isPending] = useTransition();
+    const [isPending, startTransition] = useTransition();
 
     const handleTabChange = (id) => {
         startTransition(() => {
@@ -16,7 +53,7 @@ const AboutSection = () => {
         <section className='text-white'>
             <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
                 <Image src="/images/about-section.jpeg" width={500} height={500} />
-                <div>
+                <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
                     <h2 className='text-4xl fond-bold text-white mb-4'>About Me</h2>
                     <p className='text-base lg:text-lg'>
                         alskdjaskdjaskalskdjkajd aslkdja dkjsdkjsl dkjksjdlka sdlkjadjs kljalksjd akjs
@@ -25,10 +62,22 @@ const AboutSection = () => {
                         asdjklajsdkjaksalsdja laksjdka sdlkjaslkdjalksjd k asdjaksdjalsdjk laksjdkajaslkdj
                         asdjklajsdkjaksalsdja laksjdka sdlkjaslkdjalksjd k asdjaksdjalsdjk laksjdkajaslkdj
                     </p>
-                    <div className='flex flex-row mt-8'>
-                        <span className='mr-3 font-semibold hover:text-white text-[#ADB7BE] border-b border-green-500'>Skills</span>
-                        <span>Education</span>
-                        <span>Experience</span>
+                    <div className='flex flex-row justify-start mt-8'>
+                        <TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}>
+                            {" "}
+                            Skills{" "}
+                        </TabButton>
+                        <TabButton selectTab={() => handleTabChange("education")} active={tab === "education"}>
+                            {" "}
+                            Education{" "}
+                        </TabButton>
+                        <TabButton selectTab={() => handleTabChange("certifications")} active={tab === "certifications"}>
+                            {" "}
+                            Certifications{" "}
+                        </TabButton>
+                    </div>
+                    <div className='mt-8'>
+                        {TAB_DATA.find((t) => t.id === tab).content}
                     </div>
                 </div>
             </div>
