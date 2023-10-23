70dc17781e2058fe19675b6c8d5a5e8dd1a3a2ea
Jackson Merma <mermaportocarreroj@gmail.com>
2023-10-23 18:23:45 -0500

    actualizando avatar y el scroll

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -25,7 +25,7 @@ const HeroSection = () => {
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{ duration: 0.5 }}
                     className="col-span-7 place-self-center text-center sm:text-left justify-self-start">
-                    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+                    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold">
                         <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-600">
                             Hello, I'm {" "}
                         </span>
@@ -71,13 +71,13 @@ const HeroSection = () => {
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{ duration: 0.5 }}
                     className="col-span-5 place-self-center mt-4 lg:mt-0">
-                    <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[250px] lg:h[250px] relative">
+                    <div className="rounded-full bg-[#181818] w-[300px] h-[300px] lg:w-[300px] lg:h[300px] relative">
                         <Image
-                            src="/images/avatar.png"
+                            src="/images/avatar2-profile.png"
                             alt="hero image"
                             className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                            width={300}
-                            height={300}
+                            width={400}
+                            height={400}
                         />
                     </div>
                 </motion.div>
