b6b8cbb64695fb7b03e0366fbd047df4d6f21fef
Jackson Merma <mermaportocarreroj@gmail.com>
2023-10-23 14:40:39 -0500

    agregando librearías para animaciones y usandolas en hero

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -2,31 +2,36 @@
 import React from "react";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
 
 const HeroSection = () => {
     return (
-        <section>
+        <section className="lg:py-16">
             <div className="grid grid-cols-1 sm:grid-cols-12">
-                <div className="col-span-7 place-self-center text-center sm:text-left">
-                    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                <motion.div
+                    initial={{ opacity: 0, scale: 0 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}
+                    className="col-span-7 place-self-center text-center sm:text-left justify-self-start">
+                    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
                         <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-600">
                             Hello, I'm {" "}
                         </span>
                         <br />
-                        <TypeAnimation 
-                        sequence={[
-                            "Jackson",
-                            1000,
-                            "Web developer",
-                            1000,
-                            "Data scientist",
-                            1000,
-                            "AR/VR developer",
-                            1000,
-                        ]}
-                        wrapper="span"
-                        speed={50}
-                        repeat={Infinity}
+                        <TypeAnimation
+                            sequence={[
+                                "Jackson",
+                                1000,
+                                "Web developer",
+                                1000,
+                                "Data scientist",
+                                1000,
+                                "AR/VR developer",
+                                1000,
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            repeat={Infinity}
                         />
                     </h1>
                     <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
@@ -40,8 +45,12 @@ const HeroSection = () => {
                             </span>
                         </button>
                     </div>
-                </div>
-                <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+                </motion.div>
+                <motion.div
+                    initial={{ opacity: 0, scale: 0 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}
+                    className="col-span-5 place-self-center mt-4 lg:mt-0">
                     <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[250px] lg:h[250px] relative">
                         <Image
                             src="/images/avatar.png"
@@ -51,7 +60,7 @@ const HeroSection = () => {
                             height={300}
                         />
                     </div>
-                </div>
+                </motion.div>
             </div>
         </section>
     );
