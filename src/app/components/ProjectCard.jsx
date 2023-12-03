29273e603d98b3c0549a75bb89065a904c7ee25c
Jackson Merma <mermaportocarreroj@gmail.com>
2023-12-02 21:39:06 -0500

    añadiendo nuevo proyecto y actualizando la opción para un link de git null como mensaje

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -6,24 +6,31 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
 
-    const notify = () => toast.info(
-        "The project has not been deployed",
+    const notify = (message) => toast.info(
+		 message,
     );
 
 	return (
 		<div>
 		<div className="h-52 md:h-72 rounded-t-xl relative group" style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}>
 		<div className='overlay flex items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 trasition-all duration-500'>
-		<Link href={gitUrl} className='h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'>
-		<CodeBracketIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white' />
-		</Link>
 
-		{previewUrl !== 'null' ? ( // Comprueba si gitUrl es diferente de "null"
+		{gitUrl !== 'null' ? ( // Comprueba si gitUrl es diferente de "null"
+			<Link href={gitUrl} className='h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'>
+			<CodeBracketIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white' />
+			</Link>
+		) : (
+			<button onClick={() => notify("This is a private project")} className='h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'>
+			<CodeBracketIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white' />
+			</button>
+		)}
+
+		{previewUrl !== 'null' ? ( // Comprueba si previewUrl es diferente de "null"
 			<Link href={previewUrl} className='h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'>
 			<EyeIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white' />
 			</Link>
 		) : (
-			<button onClick={notify} className='h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'>
+			<button onClick={() => notify("The project has not been deployed")} className='h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'>
 			<EyeIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white' />
 			</button>
 		)}
--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -7,6 +7,15 @@ import { motion, useInView } from "framer-motion";
 
 const projectsData = [
     {
+        id: 1,
+        title: "Virtual Reality game: Statistics module",
+        description: "This VR game enhances therapies for children with epileptic encephalopathy, allowing them to engage in therapeutic activities like virtual pizza-making. A notable feature is the collaborative implementation of a progress analysis module, enabling detailed monitoring of patient advancements during therapy sessions. This initiative underscores a shared commitment to improving therapies through innovative technology, offering a unique and engaging rehabilitation experience for these children.",
+        image: "/images/projects/cepivirt.png",
+        tag: ["All", "Desktop"],
+        gitUrl: "null",
+        previewUrl: "https://www.proquest.com/openview/7e21049be04640a4ca53fb9e01a36706/1?pq-origsite=gscholar&cbl=1006393",
+    },
+    {
         id: 2,
         title: "Conway's Game of Life",
         description: "Conway's Game of Life is an informative and inspirational project that explores the fascinating world of cellular automata. This project includes both 2D and 3D versions of the game, offering a unique and engaging perspective on emergent phenomena and computational experimentation. It invites exploration, creativity, and a deeper understanding of self-organizing systems, making it a valuable addition to the world of computer science and simulation.",
@@ -16,7 +25,7 @@ const projectsData = [
         previewUrl: "https://conways-game-of-life-in-3d.vercel.app/",
     },
     {
-        id: 2,
+        id: 3,
         title: "Unu Map",
         description: "UNU MAP is a web application that contains an interactive map where locations of endangered aquatic animals are marked, providing specific information about their name, scientific name, habitat, location, description, care, and an augmented reality view of the animal. This project aims to raise awareness through exploration of the Earth’s bodies of water about which aquatic animal species are in danger of becoming extinct.",
         image: "/images/projects/unuMap.png",
@@ -25,7 +34,7 @@ const projectsData = [
         previewUrl: "null",
     },
     {
-        id: 1,
+        id: 4,
         title: "Chess Game",
         description: "The chess game is a virtual platform that is part of a variety of games. Its purpose is to reinforce programming knowledge in Python through challenges in which users can solve problems to achieve the visualization of the pieces involved in these challenges. This platform has a look and mechanisms very similar to platforms like LeetCode, CodeChef, CodeForces, among others.",
         image: "/images/projects/chess-game.png",
@@ -61,6 +70,7 @@ const ProjectSection = () => {
             <div className='text-white flex flex-row justify-center items-center gap-2 py-6'>
                 <ProjectTag onClick={handleTabChange} name="All" isSelected={tag === "All"} />
                 <ProjectTag onClick={handleTabChange} name="Web" isSelected={tag === "Web"} />
+                <ProjectTag onClick={handleTabChange} name="Desktop" isSelected={tag === "Desktop"} />
             </div>
             <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
                 {filteredProjects.map((project, index) =>
